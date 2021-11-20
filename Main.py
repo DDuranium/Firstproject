7556f82bbff273c95ed86e3e7ba4357691aea2f8
Dinesh Duran Jimenez <dineshduranjimenez@gmail.com>
2021-11-20 22:44:24 +0100

    github

--- Main.py
+++ Main.py
@@ -1,1 +1,12 @@
 print("Dinesh is cool")
+
+numbers = [11, 2, 3, 6, 7, 8]
+
+
+def sum(array_of_numbers):
+    sum = 0
+    for num in range(len(array_of_numbers)):
+       sum += array_of_numbers[num]
+    return sum
+
+print(sum(numbers))
