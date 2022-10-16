a76ca3d12a74fe92fe3cfc85025eaf7958c3984e
Dinesh Duran Jimenez <dineshduranjimenez@gmail.com>
2022-10-16 12:51:19 +0200

    syntax shizzle

--- Main.py
+++ Main.py
@@ -1,2 +1,13 @@
+lijst = list(range(1, 1001))
+print(lijst)
 
-print('allahu akhbar')
+drietjes = []
+vijfjes = []
+
+for x in range(1,1001):
+    if (x%3 == 0):
+        drietjes.append(x)
+    elif (x%5 == 0):
+        vijfjes.append(x)
+print(drietjes)
+print(vijfjes)
