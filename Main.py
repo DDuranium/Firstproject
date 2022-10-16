4df4b704b7302d24a3a36118f0ed935d2f5b3fb8
Dinesh Duran Jimenez <dineshduranjimenez@gmail.com>
2022-10-16 12:58:41 +0200

    oplossing

--- Main.py
+++ Main.py
@@ -1,13 +1,19 @@
-lijst = list(range(1, 1001))
-print(lijst)
+# lijst = list(range(1, 1001))
+# print(lijst)
 
 drietjes = []
 vijfjes = []
 
-for x in range(1,1001):
-    if (x%3 == 0):
+for x in range(1,1000):
+    if x % 3 == 0:
         drietjes.append(x)
-    elif (x%5 == 0):
+    elif x % 5 == 0:
         vijfjes.append(x)
+
+somvijf = sum(vijfjes)
+somdrie = sum(drietjes)
+totaal = somvijf+somdrie
+
 print(drietjes)
 print(vijfjes)
+print(totaal)
