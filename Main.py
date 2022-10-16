d1e24b240f1ea7448eb106226006d1b9f2f68395
Dinesh Duran Jimenez <dineshduranjimenez@gmail.com>
2022-10-16 12:33:56 +0200

    flippertjes

--- Main.py
+++ Main.py
@@ -1,13 +1,1 @@
-print("Dinesh is cool en gijs ook")
-
-numbers = [11, 2, 3, 6, 7, 8]
-
-
-def sum(array_of_numbers):
-    sum = 0
-    for num in range(len(array_of_numbers)):
-       sum += array_of_numbers[num]
-    return sum
-
-print(sum(numbers))
 
