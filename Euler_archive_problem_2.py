30c76ea86d3fda1a33bcd4684044c368a4ce07f9
Dinesh Duran Jimenez <dineshduranjimenez@gmail.com>
2022-10-16 14:23:42 +0200

    oplosssing 2

--- Euler_archive_problem_2.py
+++ Euler_archive_problem_2.py
@@ -1,5 +1,28 @@
-startlist = (1, 2, 3)
+Fibonacci = [1,2]
 
-Fibonacci = []
+for idx in range(1,200):
+    
+    number = Fibonacci[idx]+Fibonacci[idx-1]
+    if number > 4000000: 
+        break
+    Fibonacci.append(number)
+    
 
-for idx 
+
+print(Fibonacci)
+
+elementnummer = len(Fibonacci)
+print(elementnummer)
+
+fibo_even = []
+
+for idx in range(0,31):
+    if Fibonacci[idx]%2 == 0:
+        fibo_even.append(Fibonacci[idx])
+        
+print(fibo_even)
+
+fibo_even_sum = sum(fibo_even)
+
+print(fibo_even_sum)
+
