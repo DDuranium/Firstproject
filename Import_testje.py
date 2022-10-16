81c8b71d508d20f15621778f78a6971d02455025
Dinesh Duran Jimenez <dineshduranjimenez@gmail.com>
2022-10-16 19:21:30 +0200

    tekstfile importeren

---
+++ Import_testje.py
@@ -0,0 +1,24 @@
+# =============================================================================
+# import os
+# import sys
+# import csv
+
+# absolutepath = os.path.abspath(__file__)
+# fileDirectory = os.path.dirname(absolutepath)
+# parentDirectory = os.path.dirname(fileDirectory)
+# sys.path.append(parentDirectory)
+# variable = []
+
+# with open('oefentekst.txt') as csv_file:
+#     csv_reader =csv.reader(csv_file, delimiter='t')
+    
+#     for data in csv_reader: 
+#         variable.append(data)
+    
+# print(variable)
+###############################################################################
+
+
+# Open a file: text
+text = open('numbers.txt', mode='r')
+
